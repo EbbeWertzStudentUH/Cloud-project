1ff1c6c6f55d73788d625df93eee9807d2b61006
Ebbe Wertz <146071304+EbbeWertzStudentUH@users.noreply.github.com>
2025-01-10 21:34:27 +0100

    Notifier kan nu user state management automatiseren

--- CORE_NOTIFIER - websocket+rest/NotificationService.py
+++ CORE_NOTIFIER - websocket+rest/NotificationService.py
@@ -5,19 +5,37 @@ import datetime
 
 class SubscriptionManager:
     def __init__(self):
-        self.topic_subscriptions:dict[str, set[str]] = dict() # topic -> set(user id)
-    def subscribe(self, topic:str, user_id:str):
+        self.topic_subscriptions: dict[tuple[str, str], set[str]] = dict() # (topic name, topic id) -> set(user id)
+        self.user_id_subscriptions: dict[str, set[tuple[str, str]]] = dict() # topic id -> set(topic name, user id)
+
+    def subscribe(self, topic:tuple[str, str], user_id:str):
+        topic_name, topic_id = topic
+        # voeg subscription toe aan de map
         if topic not in self.topic_subscriptions:
-            self.topic_subscriptions[topic] = set()
-        self.topic_subscriptions[topic].add(user_id)
-    def unsubscribe(self, topic:str, user_id:str):
+            self.topic_subscriptions[(topic_name, topic_id)] = set()
+        self.topic_subscriptions[(topic_name, topic_id)].add(user_id)
+        # voeg subscription toe aan de reverse map
+        if user_id not in self.user_id_subscriptions:
+            self.user_id_subscriptions[topic_id] = set()
+        self.user_id_subscriptions[topic_id].add((topic_name, user_id))
+
+    def unsubscribe(self, topic:tuple[str, str], user_id:str):
+        topic_name, topic_id = topic
+        # verwijder subscription uit de map
         if topic not in self.topic_subscriptions: return
-        self.topic_subscriptions[topic].remove(user_id)
-        if len(self.topic_subscriptions[topic]) == 0:
+        self.topic_subscriptions[(topic_name, topic_id)].remove(user_id)
+        if len(self.topic_subscriptions[(topic_name, topic_id)]) == 0:
             self.topic_subscriptions.pop(topic)
-    def getSubscribedUsers(self, topic:str) -> set[str]:
+        # verwijder subscription uit de reverse map
+        if user_id in self.user_id_subscriptions:
+            self.user_id_subscriptions[topic_id].remove((topic_name, user_id))
+        if len(self.user_id_subscriptions[topic_id]) == 0:
+            self.user_id_subscriptions.pop(topic_id)
+
+    def getSubscribedUsers(self, topic:tuple[str, str]) -> set[str]:
         if topic not in self.topic_subscriptions: return set()
         return self.topic_subscriptions[topic]
+    
 
 class NotificationService:
     def __init__(self):
@@ -28,23 +46,41 @@ class NotificationService:
         @self.fast_api_app.websocket('/ws')
         async def endpoint(websocket: WebSocket): await self._handleEndpoint(websocket)
 
-    def subscribe(self, user_id:str, topics:set[str]):
+    async def subscribe(self, user_id:str, topics:set[tuple[str, str]]):
+        online_users = set() # users die dezelfde uuid hebben als de topic
+        topic_name = ''
         for topic in topics:
             self.subscriptionManager.subscribe(topic, user_id)
+            topic_name, topic_id = topic
+            if topic_id in self.client_sockets:
+                online_users.add(topic_id)
+        if len(online_users) > 0:
+            await self._send_topic_equals_user_message(user_id, online_users, topic_name)
 
-    def unsubscribe(self, user_id:str, topics:set[str]):
+    def unsubscribe(self, user_id:str, topics:set[tuple[str, str]]):
         for topic in topics:
             self.subscriptionManager.unsubscribe(topic, user_id)
 
-    async def publishUpdate(self, topic:str, data):
-        await self._broadcastJSON("update", data, self.subscriptionManager.getSubscribedUsers(topic))
+    async def publishUpdate(self, topics:set[tuple[str, str]], update):
+        for topic in topics:
+            await self._broadcastJSON("update", update, self.subscriptionManager.getSubscribedUsers(topic))
 
-    async def publishNotification(self, topic:str, message:str):
+    async def publishNotification(self, topics:set[tuple[str, str]], notification):
         timestamp = datetime.datetime.now().strftime("%H:%M")
-        data = {"message": message, "time":timestamp}
-        await self._broadcastJSON("notification", data, self.subscriptionManager.getSubscribedUsers(topic))
+        data = {"message": notification.message, "time":timestamp}
+        for topic in topics:
+            await self._broadcastJSON("notification", data, self.subscriptionManager.getSubscribedUsers(topic))
+    
+    async def sendUpdate(self, user_id:str, update):
+        await self._broadcastJSON("update", update, [user_id])
+
+    async def sendNotification(self, user_id:str, notification):
+        timestamp = datetime.datetime.now().strftime("%H:%M")
+        data = {"message": notification.message, "time":timestamp}
+        await self._broadcastJSON("notification", data, [user_id])
 
     async def _handleEndpoint(self, websocket: WebSocket):
+        user_id = None
         await websocket.accept()
         await websocket.send_json({"type":"connection_status", "data":{"message":"Connected. Send token to authorise connection.", "ok":True}})
         try:
@@ -54,11 +90,14 @@ class NotificationService:
                 if message and not authenticated:
                     authenticated, user_id = self._handle_registration(message, websocket)
                     await websocket.send_json({"type":"connection_status", "data":{"message":f"Connection fully registered. UserID:{user_id}", "ok":True}})
+                    # update degenen die naar jou subscribed zijn (hun topic id = jouw user_id)
+                    await self._notify_other_users_about_status_change(user_id, "online")
                 else:
                     await websocket.send_json({"type":"connection_status", "data":{"message":f"This socket only accepts one message containing your authentication token.", "ok":False}})
 
         except WebSocketDisconnect:
-            if user_id in self.client_sockets:
+            if user_id and user_id in self.client_sockets:
+                await self._notify_other_users_about_status_change(user_id, "offline")
                 self.client_sockets.pop(user_id)
             print(f"Client {user_id} disconnected")
 
@@ -70,6 +109,27 @@ class NotificationService:
             except Exception as e:
                 print(f"Error sending message to {user_id}: {e}")
 
+    async def _send_topic_equals_user_message(self, user_id:str, topic_ids:set[str], topic_name:str):
+        message = {
+            "message":"You are subscribing to a topic which contains active user id's",
+            "topic":topic_name,
+            "users":[{"id":id, "status":"online"} for id in topic_ids]
+        }
+        await self._broadcastJSON("subscribed_users_status", message, [user_id])
+
+    async def _notify_other_users_about_status_change(self, user_id:str, status:str):
+        if user_id in self.subscriptionManager.user_id_subscriptions:
+            topic_and_user_set = self.subscriptionManager.user_id_subscriptions[user_id]
+            for topic_and_user in topic_and_user_set:
+                topic_name, other_user = topic_and_user
+                message = {
+                    "message":"You are subscribed to a topic which contains a user id that changed status",
+                    "topic":topic_name,
+                    "users":{"id":user_id, "status":status},
+                }
+                await self._broadcastJSON("subscribed_to_users", message, [other_user])
+        
+
     def _handle_registration(self, token:str, websocket:WebSocket) -> tuple[bool, str]:
         try:
             jwt_data = jwt.decode(token, os.getenv('JWT_SECRET'), algorithms=['HS256'])
@@ -78,4 +138,3 @@ class NotificationService:
             print(f"Client {user_id} connected and authenticated")
             return True, user_id
         except: return False, ''
-
--- CORE_NOTIFIER - websocket+rest/RestCommander.py
+++ CORE_NOTIFIER - websocket+rest/RestCommander.py
@@ -3,31 +3,38 @@ from pydantic import BaseModel
 from NotificationService import NotificationService
 
 
-class MultiTopicJSON(BaseModel):
-    name: str
-    ids: list[str]
-
 class TopicJSON(BaseModel):
-    name: str
-    id: str
+    name: str   # friends / projects
+    ids: list[str] # user id's project id's
 
-class SubscriptionJSON(BaseModel):
-    user_id: str
-    topic: MultiTopicJSON
 
 class UpdateJSON(BaseModel):
     type: str # edit/ status change / delete / new
     subject: str # edited/new/changed object id
     data: str # wat new? wat changed?
 
-class UpdateMessageJSON(BaseModel):
-    topic: MultiTopicJSON
+class NotificationJSON(BaseModel):
+    message: str
+
+class NotificationSendRequestJSON(BaseModel):
+    user_id: str
+    notification: NotificationJSON
+
+class UpdateSendRequestJSON(BaseModel):
+    user_id: str
     update: UpdateJSON
 
-
-class NotificationJSON(BaseModel):
+class NotificationPublishRequestJSON(BaseModel):
     topic: TopicJSON
-    message: str
+    notification: NotificationJSON
+
+class UpdatePublishRequestJSON(BaseModel):
+    topic: TopicJSON
+    update: UpdateJSON
+
+class SubscriptionRequestJSON(BaseModel):
+    user_id: str
+    topic: TopicJSON
 
 
 class RestCommander:
@@ -35,37 +42,49 @@ class RestCommander:
         self.notificationService = notificationService
         self.fast_api_app = FastAPI()
 
+        #  subscriben voor topics
         @self.fast_api_app.post("/subscribe")
-        def subscribe(data: SubscriptionJSON): return self.subscribe(data)
+        async def subscribe(data: SubscriptionRequestJSON): return await self.subscribe(data)
         @self.fast_api_app.delete("/subscribe")
-        def unsubscribe(data: SubscriptionJSON): return self.unsubscribe(data)
+        def unsubscribe(data: SubscriptionRequestJSON): return self.unsubscribe(data)
+        #  messages naar subscribed users voor topic
         @self.fast_api_app.post("/publish/notification")
-        async def publish_notification(data: NotificationJSON): return await self.publishNotification(data)
+        async def publish_notification(data: NotificationPublishRequestJSON): return await self.publishNotification(data)
         @self.fast_api_app.post("/publish/update")
-        async def publishUpdate(data:UpdateMessageJSON): return await self.publishUpdate(data)
+        async def publish_pdate(data:UpdatePublishRequestJSON): return await self.publishUpdate(data)
+        # message naar specifieke user
+        @self.fast_api_app.post("/send/notification")
+        async def send_notification(data: NotificationSendRequestJSON): return await self.sendNotification(data)
+        @self.fast_api_app.post("/send/update")
+        async def send_update(data: UpdateSendRequestJSON): return await self.sendUpdate(data)
 
-    def subscribe(self, data:SubscriptionJSON):
-        flat_topics = self._flattenMultiTopic(data.topic)
-        self.notificationService.subscribe(data.user_id, flat_topics)
+    async def subscribe(self, data:SubscriptionRequestJSON):
+        flat_topics = self._tuplifyTopic(data.topic)
+        await self.notificationService.subscribe(data.user_id, flat_topics)
         return {"message":f"subscribed user {data.user_id} to topic", "topic":data.topic}
     
-    def unsubscribe(self, data:SubscriptionJSON):
-        flat_topics = self._flattenMultiTopic(data.topic)
+    def unsubscribe(self, data:SubscriptionRequestJSON):
+        flat_topics = self._tuplifyTopic(data.topic)
         self.notificationService.unsubscribe(data.user_id, flat_topics)
         return {"message":f"unsubscribed user {data.user_id} from topic", "topic":data.topic}
 
-    async def publishNotification(self, data:NotificationJSON):
-        topic = self._flattenTopic(data.topic)
-        await self.notificationService.publishNotification(topic, data.message)
+    async def publishNotification(self, data:NotificationPublishRequestJSON):
+        topic = self._tuplifyTopic(data.topic)
+        await self.notificationService.publishNotification(topic, data.notification)
         return {"message":f"sent notification to users subscribed to topic", "topic":data.topic}
     
-    async def publishUpdate(self, data:UpdateMessageJSON):
-        topic = self._flattenTopic(data.topic)
+    async def publishUpdate(self, data:UpdatePublishRequestJSON):
+        topic = self._tuplifyTopic(data.topic)
         await self.notificationService.publishUpdate(topic, data.update)
         return {"message":f"sent updates to users subscribed to topic", "topic":data.topic}
+    
+    async def sendNotification(self, data:NotificationSendRequestJSON):
+        await self.notificationService.sendNotification(data.user_id, data.notification)
+        return {"message":f"sent notification to user {data.user_id}"}
+    
+    async def sendUpdate(self, data:UpdateSendRequestJSON):
+        await self.notificationService.sendUpdate(data.user_id, data.update)
+        return {"message":f"sent update to user {data.user_id}"}
 
-    def _flattenTopic(self, topic:TopicJSON) -> str:
-        return f"{topic.name}_{topic.id}"
-
-    def _flattenMultiTopic(self, topic:MultiTopicJSON) -> list[str]:
-        return [f"{topic.name}_{id}" for id in topic.ids]
+    def _tuplifyTopic(self, topic:TopicJSON) -> list[tuple[str, str]]:
+        return [(topic.name,id) for id in topic.ids]
