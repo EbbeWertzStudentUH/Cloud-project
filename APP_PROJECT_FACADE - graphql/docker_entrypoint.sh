f652298ba8e3c513b2a65e53e84b54da11a3d9e1
Ebbe Wertz <146071304+EbbeWertzStudentUH@users.noreply.github.com>
2025-01-12 19:54:14 +0100

    project facade initial docker setup

---
+++ APP_PROJECT_FACADE - graphql/docker_entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "=== Docker Container Info ==="
+echo "container: APP_PROJECT_FACADE-GRAPHQL"
+echo "service: app-project-facade-graphql"
+echo "poort : 3009"
+echo "==============================="
+echo
+echo "=== content van /app ==="
+ls /app
+echo
+echo "=== Server start command: ==="
+echo "..."
+
+# tail -f /dev/null
