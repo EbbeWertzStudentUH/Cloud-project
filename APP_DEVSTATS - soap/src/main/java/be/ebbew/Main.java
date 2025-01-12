e2eea72de53fe5e72749924c14e0934c4de1ff37
Ebbe Wertz <146071304+EbbeWertzStudentUH@users.noreply.github.com>
2025-01-12 10:20:40 +0100

    refactor

--- APP_DEVSTATS - soap/src/main/java/be/ebbew/Main.java
+++ APP_DEVSTATS - soap/src/main/java/be/ebbew/Main.java
@@ -7,19 +7,17 @@ import jakarta.xml.ws.Endpoint;
 
 import java.time.Instant;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
+
 import io.github.cdimascio.dotenv.Dotenv;
 
 
 @WebService
 public class Main {
 
-    private final ConcurrentHashMap<String, AtomicInteger> outgoingRequests = new ConcurrentHashMap<>();
     private final ConcurrentHashMap<String, List<Long>> requestTimestamps = new ConcurrentHashMap<>();
 
 
@@ -28,44 +26,17 @@ public class Main {
                                         @WebParam(name = "identifier") String identifier,
                                         @WebParam(name = "serviceName") String serviceName) {
         String key = serviceName + ":" + serviceType + ":" + identifier;
-        outgoingRequests.computeIfAbsent(key, k -> new AtomicInteger(0)).incrementAndGet();
         requestTimestamps.computeIfAbsent(key, k -> Collections.synchronizedList(new ArrayList<>()))
                 .add(Instant.now().toEpochMilli());
     }
 
     @WebMethod
     public OutgoingRequestCountsResponse getOutgoingRequestCounts() {
-        Map<String, Integer> countsMap = outgoingRequests.entrySet().stream()
-                .collect(ConcurrentHashMap::new,
-                        (map, entry) -> map.put(entry.getKey(), entry.getValue().get()),
-                        ConcurrentHashMap::putAll);
-
-        Map<String, Integer> rpmMap = new HashMap<>();
-        long currentTime = Instant.now().toEpochMilli();
-        long oneMinuteMillis = 60000L;
-        double decayFactor = 0.9;
-        for (Map.Entry<String, List<Long>> entry : requestTimestamps.entrySet()) {
-            String key = entry.getKey();
-            List<Long> timestamps = entry.getValue();
-            double rpm = 0.0;
-            for (Long timestamp : timestamps) {
-                long ageMillis = currentTime - timestamp;
-                if (ageMillis <= oneMinuteMillis) {
-                    double fractionalContribution = (oneMinuteMillis - ageMillis) / (double) oneMinuteMillis;
-                    rpm += fractionalContribution;
-                } else {
-                    double decayContribution = Math.pow(decayFactor, (ageMillis - oneMinuteMillis) / 1000.0);
-                    rpm += decayContribution;
-                }
-            }
-            rpmMap.put(key, (int) Math.round(rpm)); // Store the calculated RPM for the key
-        }
-
+        Map<String, Integer> countsMap = Util.calculateCounts(requestTimestamps);
+        Map<String, Integer> rpmMap = Util.calculateRMP(requestTimestamps);
         return new OutgoingRequestCountsResponse(countsMap, rpmMap);
     }
-
-
-
+    
 
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.configure().load();
---
+++ APP_DEVSTATS - soap/src/main/java/be/ebbew/Util.java
@@ -0,0 +1,43 @@
+package be.ebbew;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Util {
+
+    public static Map<String, Integer> calculateCounts(ConcurrentHashMap<String, List<Long>> requestTimestamps){
+        Map<String, Integer> countsMap = requestTimestamps.entrySet().stream()
+                .collect(ConcurrentHashMap::new,
+                        (map, entry) -> map.put(entry.getKey(), entry.getValue().size()),
+                        ConcurrentHashMap::putAll);
+        return countsMap;
+    }
+
+
+    public static Map<String, Integer> calculateRMP(ConcurrentHashMap<String, List<Long>> requestTimestamps){
+        Map<String, Integer> rpmMap = new HashMap<>();
+        long currentTime = Instant.now().toEpochMilli();
+        long oneMinuteMillis = 60000L;
+        double decayFactor = 0.9;
+        for (Map.Entry<String, List<Long>> entry : requestTimestamps.entrySet()) {
+            String key = entry.getKey();
+            List<Long> timestamps = entry.getValue();
+            double rpm = 0.0;
+            for (Long timestamp : timestamps) {
+                long ageMillis = currentTime - timestamp;
+                if (ageMillis <= oneMinuteMillis) {
+                    double fractionalContribution = (oneMinuteMillis - ageMillis) / (double) oneMinuteMillis;
+                    rpm += fractionalContribution;
+                } else {
+                    double decayContribution = Math.pow(decayFactor, (ageMillis - oneMinuteMillis) / 1000.0);
+                    rpm += decayContribution;
+                }
+            }
+            rpmMap.put(key, (int) Math.round(rpm)); // Store the calculated RPM for the key
+        }
+        return rpmMap;
+    }
+}
